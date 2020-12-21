a94fe53dd7d6de2c51b00d896df1269c354428ca
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-12-21 08:09:55 -0800

    commit tree 4ac93615c6e8031d42cb855d1451e4fa6b69c8fe

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,16 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :posts
+    validates_uniqueness_of :name
+    validates :age, numericality: {greater_than: 0}
+    validates :bio, length: {minimum: 30}
 
+    def total_likes
+        self.posts.sum{|post| post.likes }
+    end
+    
+    def featured_post
+        self.posts.max_by{|post| post.likes}
+    end
+    
 end
